9402e88652b6a4336ec8aef72810a50c38f3ea33
Eduardo Dias <edu_rodriguesdias@hotmail.com>
2017-09-24 04:16:36 -0300

    Configuraçao do Firebase + Tela de cadastro + SweetAlert Message - Pendente finalizações

--- app/src/main/java/stringdevelopment/com/br/gopizza/MainActivity.java
+++ app/src/main/java/stringdevelopment/com/br/gopizza/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btnUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/stringdevelopment/com/br/gopizza/NewUser.java
+++ app/src/main/java/stringdevelopment/com/br/gopizza/NewUser.java
@@ -1,13 +1,75 @@
 package stringdevelopment.com.br.gopizza;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class NewUser extends AppCompatActivity {
+
+    private Button btnBack;
+
+    private Button btnConfirm;
+
+    private FirebaseAuth authentication;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+
+        btnBack = (Button) findViewById(R.id.btn_back);
+
+        btnConfirm = (Button) findViewById(R.id.btn_confirm);
+
+
+
+        //Insere novo usuário BD Firebase
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                authentication = FirebaseAuth.getInstance();
+
+                //CADASTRO
+                authentication.createUserWithEmailAndPassword("edu_rodriguesdias@hotmail.com", "x3f96771")
+                        .addOnCompleteListener(NewUser.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    //SUCESSO AO CADASTRAR
+                                   new  SweetAlertDialog(NewUser.this, SweetAlertDialog.SUCCESS_TYPE)
+                                           .setTitleText("Sucesso!")
+                                           .setContentText("Usuário criado com sucesso!").show();
+                                }else{
+                                    new  SweetAlertDialog(NewUser.this, SweetAlertDialog.ERROR_TYPE)
+                                            .setTitleText("Opss...!")
+                                            .setContentText("Erro ao cadastrar usuário! Tente novamente.").show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        //Volta para a tela de login
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NewUser.this, MainActivity.class));
+            }
+        });
+
     }
 }
