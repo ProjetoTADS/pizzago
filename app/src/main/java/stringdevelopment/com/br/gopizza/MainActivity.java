eda840b50e9ceb2dc5e313dceea92daba7bb7e66
Eduardo Dias <edu_rodriguesdias@hotmail.com>
2017-09-24 00:00:06 -0300

    Layout tela de cadastro

--- app/src/main/java/stringdevelopment/com/br/gopizza/MainActivity.java
+++ app/src/main/java/stringdevelopment/com/br/gopizza/MainActivity.java
@@ -1,13 +1,29 @@
 package stringdevelopment.com.br.gopizza;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnUser = (Button) findViewById(R.id.btn_user);
+
+        btnUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity.this, NewUser.class));
+            }
+        });
     }
+
 }
