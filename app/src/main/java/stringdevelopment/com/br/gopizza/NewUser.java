fdb88505c174efd832ad7b04442b41e63b61d387
Eduardo Dias <edu_rodriguesdias@hotmail.com>
2017-09-26 22:41:38 -0300

    Cadastro de usuário Firebase finalizado

--- app/src/main/java/stringdevelopment/com/br/gopizza/NewUser.java
+++ app/src/main/java/stringdevelopment/com/br/gopizza/NewUser.java
@@ -1,11 +1,13 @@
 package stringdevelopment.com.br.gopizza;
 
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -24,6 +26,12 @@ public class NewUser extends AppCompatActivity {
 
     private FirebaseAuth authentication;
 
+    private EditText email;
+
+    private EditText password;
+
+    private  EditText name;
+
 
 
     @Override
@@ -35,31 +43,66 @@ public class NewUser extends AppCompatActivity {
 
         btnConfirm = (Button) findViewById(R.id.btn_confirm);
 
+        email = (EditText) findViewById(R.id.txtEmail);
+
+        password = (EditText) findViewById(R.id.txtPassword);
+
+        name = (EditText) findViewById(R.id.txtNome);
 
 
         //Insere novo usuário BD Firebase
         btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                authentication = FirebaseAuth.getInstance();
+                final String usermail = email.getText().toString();
+                final String userpass = password.getText().toString();
+                final String username = name.getText().toString();
 
-                //CADASTRO
-                authentication.createUserWithEmailAndPassword("edu_rodriguesdias@hotmail.com", "x3f96771")
-                        .addOnCompleteListener(NewUser.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful()){
-                                    //SUCESSO AO CADASTRAR
-                                   new  SweetAlertDialog(NewUser.this, SweetAlertDialog.SUCCESS_TYPE)
-                                           .setTitleText("Sucesso!")
-                                           .setContentText("Usuário criado com sucesso!").show();
-                                }else{
-                                    new  SweetAlertDialog(NewUser.this, SweetAlertDialog.ERROR_TYPE)
-                                            .setTitleText("Opss...!")
-                                            .setContentText("Erro ao cadastrar usuário! Tente novamente.").show();
+                if(usermail.isEmpty() || userpass.isEmpty() || username.isEmpty()){
+                    new  SweetAlertDialog(NewUser.this, SweetAlertDialog.ERROR_TYPE)
+                            .setTitleText("Opss...!")
+                            .setContentText("Preencha todos os campos antes de continuar!")
+                            .show();
+                }else{
+                    authentication = FirebaseAuth.getInstance();
+
+
+                    //CADASTRO
+                    authentication.createUserWithEmailAndPassword(usermail, userpass)
+                            .addOnCompleteListener(NewUser.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        //SUCESSO AO CADASTRAR
+                                        new SweetAlertDialog(NewUser.this, SweetAlertDialog.SUCCESS_TYPE)
+                                                .setTitleText("Sucesso!")
+                                                .setContentText("Usuário cadastrado!")
+                                                .setConfirmText("Yes,delete it!")
+                                                .showCancelButton(true)
+                                                .setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                                    @Override
+                                                    public void onClick(SweetAlertDialog sDialog) {
+                                                        Intent intent = new Intent(NewUser.this, permitionScreen.class);
+
+                                                        startActivity(intent);
+                                                        finish();
+                                                    }
+                                                })
+                                                .show();
+                                    }else{
+                                        //ERRO AO CADASTRAR
+
+                                        new  SweetAlertDialog(NewUser.this, SweetAlertDialog.ERROR_TYPE)
+                                                .setTitleText("Opss...!")
+                                                .setContentText("Erro ao cadastrar usuário: "+usermail+" com senha: "+userpass+"! Tente novamente. Error: ")
+                                                .show();
+                                    }
                                 }
-                            }
-                        });
+                            });
+                }
+
+
+
             }
         });
 
---
+++ app/src/main/java/stringdevelopment/com/br/gopizza/permitionScreen.java
@@ -0,0 +1,13 @@
+package stringdevelopment.com.br.gopizza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class permitionScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permition_screen);
+    }
+}
